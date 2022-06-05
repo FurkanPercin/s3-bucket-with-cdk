ecf248ad3a159621edd27a835ebb95d3d5be31ae
Furkan Perçin <furkanpercin@hotmail.com>
2022-06-05 16:11:44 +0300

    Creation of S3 Stack

--- bin/s3-cdk.ts
+++ bin/s3-cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { S3CdkStack } from '../lib/s3-cdk-stack';
-
+import { S3BucketStack } from '../lib/s3-bucket-stack';
 const app = new cdk.App();
 new S3CdkStack(app, 'S3CdkStack');
+// Creating an S3 bucket stack
+const s3_bucket_stack = new S3BucketStack(app, 'patikaS3Stack');
